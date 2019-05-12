ee5fcb08b512239539a20e2bae6495106bfde7e4
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-05-13 03:18:29 +0800

    Fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,34 +9,34 @@ class ProductsController < ApplicationController
   end
 
   # GET /products/1
-  # def show
-  #   render json: @product
-  # end
+  def show
+    render json: @product
+  end
 
   # POST /products
-  # def create
-  #   @product = Product.new(product_params)
+  def create
+    @product = Product.new(product_params)
 
-  #   if @product.save
-  #     render json: @product, status: :created, location: @product
-  #   else
-  #     render json: @product.errors, status: :unprocessable_entity
-  #   end
-  # end
+    if @product.save
+      render json: @product, status: :created, location: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
 
   # PATCH/PUT /products/1
-  # def update
-  #   if @product.update(product_params)
-  #     render json: @product
-  #   else
-  #     render json: @product.errors, status: :unprocessable_entity
-  #   end
-  # end
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
 
   # DELETE /products/1
-  # def destroy
-  #   @product.destroy
-  # end
+  def destroy
+    @product.destroy
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
