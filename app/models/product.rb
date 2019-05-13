fd885f80e72595c72b76067d87f8fe8fb6a15a87
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-05-13 19:41:54 +0800

    Fix indent

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-    has_many :order_details
-    has_many :orders, through: :order_details
+  has_many :order_details
+  has_many :orders, through: :order_details
 end
