a352ae59e0135ff74f744b0737c2853d2ccadcb9
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-05-14 02:09:11 +0800

    Add validates to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   has_many :order_details
   has_many :orders, through: :order_details
+
+  validates :inventory, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :sold, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :price, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
