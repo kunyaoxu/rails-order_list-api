e43cbb87ade379bdc88da293186428362debe054
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-05-16 03:46:06 +0800

    Add data length validate

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :products, through: :products
+
+  validates :name, :address, :email, :order_details, length: {minimum:1}
 end
